73a2044eb625746d613f893fc34e15aebb63c90c
Jacob Corum <cor15031@byui.edu>
2020-11-21 17:24:18 -0700

    mvp for header and footer, some home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import About from "./pages/About";
+// import Blog from "./pages/Blog";
+// import About from "./pages/About";
 import Paintings from "./pages/Paintings";
-import NoMatch from "./pages/NoMatch";
+// import NoMatch from "./pages/NoMatch";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -17,12 +16,11 @@ function App() {
         <Header />
         <div className="container">
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/blog" component={Blog} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/paintings" component={Paintings} />
-
-            <Route exact path="/nomatch" component={NoMatch} />
+            {/* <Route exact path="/" component={Home} /> */}
+            {/* <Route exact path="/blog" component={Blog} /> */}
+            {/* <Route exact path="/about" component={About} /> */}
+            <Route exact path="/" component={Paintings} />
+            {/* <Route exact path="/nomatch" component={NoMatch} /> */}
           </Switch>
         </div>
         <Footer />
